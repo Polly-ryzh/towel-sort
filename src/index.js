4a3a6ef884b52fa285a4db57cabccf73a0075860
Polly-ryzh <ryzhkova.polinka@yandex.ru>
2023-02-27 02:06:13 +0500

    feat: function towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  let arrayOfNumbers = [];
+  if(matrix)
+    for(let i = 0; i < matrix.length; i++){
+      getValue(matrix[i], arrayOfNumbers, i % 2 == 1);
+    }
+
+  return arrayOfNumbers;
+}
+
+function getValue(matrix, arrayOfNumbers, isReversed){
+  if (!isReversed){
+    for (let i = 0; i < matrix.length; i++){
+      arrayOfNumbers.push(matrix[i]);
+    }
+  } else {
+    for (let i = matrix.length-1; i>=0; i--){
+      arrayOfNumbers.push(matrix[i]);
+    }
+  }
 }
